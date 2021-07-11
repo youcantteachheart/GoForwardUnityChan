005bf6165c84752e97a80747d245ac57d986812f
Liam Neeson <shabekuri7@naver.com>
2021-07-11 19:46:58 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public AudioClip block;
+    AudioSource audioSource;
+
+    private float speed = -12;
+    private float deadLine = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag")
+        {
+            Debug.Log("isGround is true");
+            audioSource.PlayOneShot(block);
+        }
+       
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject gameOverText;
+    private GameObject runLengthText;
+
+    private float len = 0;
+    private float speed = 5f;
+
+    private bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.isGameOver == false)
+        {
+            this.len += this.speed * Time.deltaTime;
+            this.runLengthText.GetComponent<Text>().text = "Distance: " + len.ToString("F2") + "m";
+        }
+
+        if(this.isGameOver == true)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        this.gameOverText.GetComponent<Text>().text = "Game Over";
+        this.isGameOver = true;
+    }
+}
